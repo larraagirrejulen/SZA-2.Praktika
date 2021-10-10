87f534f499532eaca87803962c2c98efc51b82ce
Julen <larraagirrejulen@gmail.com>
2021-10-10 19:34:04 +0200

    Update zerbitzari.py

--- zerbitzari.py
+++ zerbitzari.py
@@ -1,6 +1,13 @@
-import socket, os, signal
+import socket, os, signal, re
 
 PORT = 50000
+
+function dataEtaOrduaEgiaztatu(dataEtaOrdua):
+	pattern = re.compile("^(19|20)[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[01])([01][0-9]|2[0-3])([0-5][0-9]){2}$")
+	if pattern.match(dataEtaOrdua)
+		return dataEtaOrdua
+	else:
+		return None
 
 s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 
@@ -17,29 +24,43 @@ while True:
 	else:
 		s.close()
 		while True:
-			buf = elkarrizketa.recv( 1024 )
+			buf = elkarrizketa.recv( 1024 ).decode()
 			if not buf:
 				break
 			komandoa = buf[0:2:1]
-			if(komandoa=="DIR"){
+			if komandoa=="DIR":
+				erantzuna = ""
                 norabidea = buf[3:end:1]
-                if ((norabidea[0] == '-' or norabidea[0] == '+') and (norabidea[1:end:1]>0 and norabidea[1:end:1]<9999)){
+                if (norabidea[0] == '-' or norabidea[0] == '+') and (int(norabidea[1:end:1])>0 and int(norabidea[1:end:1])<9999):
                     #datu-basetik irudia hartzen du eta bere data eta ordua bultatzen du.
                     #errore 06 bueltatzen du emandako norabidearekin irudirik ez badago.
-                    dataEtaOrdua = " "
-                    elkarrizketa.sendall(dataEtaOrdua.encode())
-                }else{
-                    #errore 05 bueltatzen du emandako parametroak txarto idatzita badaude
-                }
-			}else if(komandoa=="TME"){
+                    if True:
+	                    dataEtaOrdua = " "
+	                    erantzuna = "OK+" + dataEtaOrdua
+                	else:
+                		erantzuna = "ER-06"
+                else:
+                    erantzuna = "ER-05"
+                elkarrizketa.sendall(erantzuna.encode())
+			else if(komandoa=="TME"):
+				erantzuna = ""
+				if dataEtaOrduaEgiaztatu(buf[3:end:1]) is None:
+					erantzuna = "ER-05"
+				else:
+					#DBan begiratu ez badago 07 errore kodea
+					if True:
+						norabidea = " "
+						erantzuna = "OK+" + norabidea
+					else:
+						erantzuna = "ER-07"
+				elkarrizketa.sendall(erantzuna.encode())
+			else if(komandoa=="IMG"):
 
-			}else if(komandoa=="IMG"){
-
-			}else if(komandoa=="QTY"){
+			else if(komandoa=="QTY"):
 				
-			}else{
-				#errore 02 komando ezezaguna
-			}
+			else:
+				elkarrizketa.sendall("ER-02".encode())
+		
 		print( "Konexioa ixteko eskaera jasota." )
 		elkarrizketa.close()
 		exit( 0 )
